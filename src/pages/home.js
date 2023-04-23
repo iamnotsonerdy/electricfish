2d7d161cc214adc1fa3a6d09d2df81ecfb82a953
iamnotsonerdy <131611041+iamnotsonerdy@users.noreply.github.com>
2023-04-23 22:53:39 +0530

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,7 @@ export default function Homepage() {
       <div>
 
         <div className='h-[100vh] w-full'>
-          <h1 className='text-6xl px-10 py-10 heading'>ELECTRIC<br/>FISHES</h1>
+          <h1 className='text-6xl px-10 py-10 heading'>ELECTRIC<br/>FISH</h1>
         </div>
 
         <div className='h-[100vh] heading'>
@@ -27,11 +27,9 @@ export default function Homepage() {
 
           <HashLink className='text-4xl px-10 py-5 float-left' to="#how">
             <div className='rounded-full border-2 border-black aspect-square grid place-items-center'>
-              <p className='circle_type'>How?</p>
+              <p className=''>How?</p>
             </div>
           </HashLink>
-
-          
 
           <HashLink className='text-4xl px-10 py-5 float-left' to="#exp">
             <div className='rounded-full border-2 border-black aspect-square grid place-items-center'>
@@ -45,9 +43,7 @@ export default function Homepage() {
 
         <div className='h-[100vh]' id='hist'>
           <h2 className='text-4xl px-10 py-10 heading'>History</h2>
-          {//p className='px-10'>timeline goes here</p>
-          }
-          <iframe title="timeline" src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1gdrUPByUq0VuKsgNHNihEd1zbXTFQzmz4uOOePT38_w&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+          <p className='px-10'>timeline goes here</p>
         </div>
         <div className='h-[100vh]' id='where'>
           <h2 className='text-4xl px-10 py-10 heading'>Where?</h2>
